0f814a65f1cd4cb82e4bb2051346e5de181c19af
zpbc007 <719197352@qq.com>
2024-04-23 09:04:25 +0800

    Initial Commit

---
+++ agc-test/agc_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  agc_testApp.swift
+//  agc-test
+//
+//  Created by zhaopeng on 2024/4/23.
+//
+
+import SwiftUI
+
+@main
+struct agc_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
